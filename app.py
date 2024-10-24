fbf3c08183f589aa4e89879264ea398a39e95c4a
Sahil Mhatre <shorenex12@gmail.com>
2024-10-25 03:05:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'checking the Argocd working or not'
+    return 'checking the Argocd and jenkins are working properly or not'
