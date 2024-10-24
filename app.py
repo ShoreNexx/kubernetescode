a3e90f51ba947df2e90bc7352464fedbbc7ec30c
Sahil Mhatre <shorenex12@gmail.com>
2024-10-25 03:35:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+
+app = Flask(__name__, static_folder='shoe-frontend', template_folder='shoe-frontend')
 
 @app.route('/')
-def hello_world():
-    return 'checking if it is working or not'
+def index():
+    return render_template('index.html')
